ad53c4ceddd243870c14972757d45ee1fe36231d
MACBOOK <c_mai3@fanshaweonline.ca>
2018-03-09 20:09:30 -0500

    whatever

---
+++ theApp/config.js
@@ -0,0 +1,10 @@
+var config = {
+  user: "root",
+  password: "root", //blank fr windows
+  port: 8889,
+  //port: 3306,
+  host: "localhost",
+  database: "rokuVideos"
+}
+
+module.exports = config;
---
+++ videos.md
@@ -0,0 +1,1 @@
+https://drive.google.com/drive/folders/19J3QhE62j2-oPEyFqqr8LDXVxlrLbGMd?usp=sharing
